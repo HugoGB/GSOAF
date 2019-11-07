5484a3899e152fb0bc639ed6f632e7e5f051c32a
Hugo GRAINVILLE--BERTRAN <hugogb@outlook.fr>
2019-11-07 15:09:52 +0100

    'poulet'

--- screens/CreateParty.js
+++ screens/CreateParty.js
@@ -1,30 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import PickerPlayers from '../components/PickerPlayers.js';
 
-export default function CreateParty() {
-  return (
-    <View style={styles.container}>
-        <Image style={styles.image}
-            source={require('../assets/logo_unique.png')} />
-        <Text style={styles.title}>G S O A F</Text>        
-    </View>
-  );
-}
-
-// creation variable Partie
-class Partie {
-    
-}
-// Ajouter joueurs  
-//Questions(4) pour distribution de cartes
-//montage pyramide 
-
-const styles = StyleSheet.create({
-    container:{
-        display: 'flex' ,
-        alignItems: 'center',
-        justifyContent: 'space-between'
+export default class CreateParty extends React.Component {
+  static navigationOptions = {
+    headerTintColor: 'white',
+    headerStyle: {
+      backgroundColor: '#114147'
     },
+  };
+  render() {    
+    const {navigate} = this.props.navigation;
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image}
+                source={require('../assets/logo_unique.png')} />
+            <Text style={styles.title}>G S O A F</Text>
+            <PickerPlayers /> 
+            <View style={{width:250, marginTop:50}}>
+                <Button
+                    color="#FFB341"
+                    title="Jouer"
+                    onPress={() => navigate('Accueil')} />
+            </View>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({    
+    container: {
+        flex: 1,
+        backgroundColor: '#195C65',
+        alignItems: 'center',
+        textAlign: 'center',
+    },    
     title: {
         color: 'white',
     },
@@ -32,11 +42,7 @@ const styles = StyleSheet.create({
         width: 80,
         height: 75,
         left: 12,
-        marginTop:75,
+        marginTop:25,
         resizeMode: 'stretch',
-        // marginTop:75,
-        // width: 200,
-        // height: 75,
-        // resizeMode: 'stretch'
     },
 });
